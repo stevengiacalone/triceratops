7d05fbda2a5495aeca3bdd01ebb6eb11f336dd11
Steven Giacalone <steven_giacalone@berkeley.edu>
2020-02-20 12:29:17 -0800

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,3 @@
+Tool for Rating Interesting Candidate Exoplanets and Reliability Analysis of Transits Originating from Proximate Stars - A tool for validating TESS Objects of Interest
+
+This repository is a work in progress.
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name = "triceratops",
+      version = '0.1',
+      description = "Statistical Validation of TESS Objects of Interest",
+      long_description = readme(),
+      author = "Steven Giacalone",
+      author_email = "steven_giacalone@berkeley.edu",
+      url = "https://github.com/stevengiacalone/triceratops",
+      packages = find_packages(),
+      package_data = {'triceratops': ['data/*']},
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+   	'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Astronomy'
+        ],
+      install_requires=['numpy>=1.18.1','pandas>=0.23.4', 'scipy>=1.1.0', 'matplotlib>=3.0.3',
+                        'astropy>=3.1.2', 'astroquery>=0.4', 'batman-package>=2.4.6',
+                        'mechanicalsoup>=0.12.0'],
+      zip_safe=False
+)
