8133940dec00d01fb74c4ee432c44c251f3971a4
Steven Giacalone <steven_giacalone@berkeley.edu>
2021-02-02 14:58:47 -0800

    v1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name = "triceratops",
-      version = '1.0.0',
+      version = '1.0.1',
       description = "Statistical Validation of TESS Objects of Interest",
       long_description = readme(),
       author = "Steven Giacalone",
